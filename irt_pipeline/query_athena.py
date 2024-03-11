494cc81a6dad1ce549ab4629a45b637cf5b79435
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-03-11 10:55:08 +0900

    Update query_athena.py

--- irt_pipeline/query_athena.py
+++ irt_pipeline/query_athena.py
@@ -7,12 +7,15 @@ def query_athena_data(conn):
     try:
         query = '''
             WITH
-            -- problem(문제 코드) 테이블 초등, 수학, 삭제 N인 문제 추출
+            -- problem(문제 코드) 테이블
+            -- 최신 교육 과정의 초등 5학년 수학 문제 중 삭제되지 않은 문제 추출
             problem_base AS(
             SELECT f_problem_id, f_difficult_cd, f_subject_id
             FROM "shine_cat"."t_problem_analysis"
             WHERE f_emh_cd ='E0' AND f_area_cd = 'MA' AND f_deleteyn ='N' AND f_subject_id IN (1982, 1983)),
-            -- testhisdtl(문제 풀이 이력) 테이블 userid, mcode, quizcode 중복값 제외
+            
+            -- testhisdtl(문제 풀이 이력) 테이블
+            -- userid와 quizcode가 중복인 데이터 모두 제외
             tbl_base AS(
             SELECT userid, quizcode, TRIM(correct) AS correct
             FROM "shine_cat"."tbl_app_testhisdtl"
@@ -24,6 +27,7 @@ def query_athena_data(conn):
                 HAVING COUNT(*) = 1
             )
             AND correct !=''),
+            
             -- INNER JOIN을 사용하여 problem_base와 tbl_base 테이블 조인
             inner_joined AS (
             SELECT
@@ -36,14 +40,18 @@ def query_athena_data(conn):
             INNER JOIN
                 tbl_base ON problem_base.f_problem_id = tbl_base.quizcode
             )
+            
             -- 문제 풀이 이력 데이터 추출
             SELECT userid, quizcode, correct, f_subject_id, f_difficult_cd
             FROM inner_joined
-            WHERE correct != '' AND (correct = 'X' OR correct ='O') '''
+            WHERE correct != '' AND (correct = 'X' OR correct ='O')
+        '''
+        
         cursor = conn.cursor()
         df = cursor.execute(query).as_pandas()
         logger.info("Query executed successfully and data fetched from Athena")
         return df
+        
     except Exception as e:
         logger.error(f"Failed to execute query and fetch data: {str(e)}")
         raise e
