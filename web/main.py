a6fe3288520832116a4752a21753d6586ed8c34f
GaYeon-Alice <kgyalice@gmail.com>
2024-03-21 14:35:07 +0900

    시뮬레이터 분기 및 문구 일부 수정

--- web/main.py
+++ web/main.py
@@ -1,28 +1,23 @@
 from fastapi import FastAPI, HTTPException, APIRouter, Request, Depends, Response
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-from sqlalchemy import text, func, Column, Integer, String, Float, create_engine
+from sqlalchemy import text, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from database import SessionLocal
-from database import check_userid_duplicate, sessionmaker, SessionLocal
 from sqlalchemy.orm import Session
-from load import load_data
-import numpy as np
-from catsim.initialization import FixedPointInitializer
-from catsim.selection import UrrySelector
-from catsim.estimation import NumericalSearchEstimator
-from catsim.stopping import MinErrorStopper
-from simulator import Simulator
-from quiz import json_to_array, get_quiz
+from simulator import SimulatorManager
 from datetime import datetime, timedelta
 import base64
+from load import load_data
 
 
 app = FastAPI()
 router = APIRouter()
 Base = declarative_base()
+
+json_data = load_data('mathcat-bucket', 'irt_result/yyyy=2024/mm=03/dd=13/irt_result.json')
 
 def get_db():
     db = SessionLocal()
@@ -31,97 +26,8 @@ def get_db():
     finally:
         db.close()
 
-
-# 초기화
-json_data = load_data('mathcat-bucket', 'irt_result/yyyy=2024/mm=03/dd=13/irt_result.json')
-result_array = json_to_array(json_data)
-initial_item_ids = get_quiz(json_data, result_array)
-    
-initializer = FixedPointInitializer(0)
-selector = UrrySelector()
-estimator = NumericalSearchEstimator()
-stopper = MinErrorStopper(0.6)
-
-class SimulatorManager:
-    def __init__(self):
-        self.simulator = None
-    
-    def initialize_simulator(self):
-        self.simulator = Simulator(np.array(result_array), 4, initializer, selector, estimator, stopper)
-
 simulator_manager = SimulatorManager()
 
-class Simulator:
-    def __init__(self, result_array, init_items, initializer, selector, estimator, stopper):
-        self.result_array = result_array
-        self.init_items = init_items
-        self.initializer = initializer
-        self.selector = selector
-        self.estimator = estimator
-        self.stopper = stopper
-        self.administered_items = []
-        self.responses = []
-        self.index = 0
-
-    def recommend_next(self, user_answer=None, quiz=False):
-        
-        ## 다음 문제 추천
-        # 초기 4문제는 리스트에서 반환
-        if len(self.administered_items) < self.init_items:
-            if quiz:
-                return initial_item_ids[self.index]
-
-            item_index = list(json_data['item_ids'].values()).index(initial_item_ids[self.index])
-            self.administered_items.append(item_index)
-
-            # 문제에 대한 응답 추가
-            correct = user_answer
-            self.responses.append(correct == True)
-            print(f'Did the user answer the item {initial_item_ids[self.index]} correctly?: {self.responses[self.index]}')
-
-
-        # 5번째 문제부터는 사용자의 학습 수준에 맞게 추천
-        else:
-            item_index = self.selector.select(items=self.result_array, administered_items=self.administered_items, est_theta=self.est_theta)
-            if quiz:
-                return json_data['item_ids'][str(item_index)]
-            
-            self.administered_items.append(item_index)
-
-            # 문제에 대한 응답 추가
-            correct = user_answer
-            self.responses.append(correct == True)
-            print(f"Did the user answer the item {json_data['item_ids'][str(item_index)]} correctly?: {self.responses[self.index]}")
-
-
-        # 사용자의 학습 수준 추정
-        if self.index == 3:
-            self.est_theta = self.initializer.initialize()
-            self.est_theta = self.estimator.estimate(
-                items=np.array(self.result_array),
-                administered_items=self.administered_items,
-                response_vector=self.responses,
-                est_theta=self.est_theta
-            )
-        elif self.index > 3:
-            self.est_theta = self.estimator.estimate(
-                items=np.array(self.result_array),
-                administered_items=self.administered_items,
-                response_vector=self.responses,
-                est_theta=self.est_theta
-            )
-        else:
-            self.est_theta = 0
-            
-        if self.stopper.stop(administered_items=np.array(self.result_array)[self.administered_items], theta=self.est_theta):
-            # 사용자의 학습 수준 출력
-            print("시험 종료")
-            print("Final estimated proficiency:", self.est_theta)
-            return self.est_theta, self.administered_items, self.responses, self.index, True
-
-        self.index += 1
-        return self.est_theta, self.administered_items, self.responses, self.index, False
-            
 
 class User(BaseModel):
     userid: str
@@ -133,6 +39,7 @@ def encode_userid(userid: str) -> str:
 def decode_userid(encoded_userid: str) -> str:
     # Base64로 인코딩된 userid를 디코딩합니다.
     return base64.urlsafe_b64decode(encoded_userid.encode('ascii')).decode('utf-8')
+
 
 @app.post("/users/")
 async def create_user(user_create: User, response: Response,  db: Session = Depends(get_db)):
@@ -150,18 +57,23 @@ async def create_user(user_create: User, response: Response,  db: Session = Depe
     decoded_userid = decode_userid(encoded_userid)
     response.set_cookie(key="userid", value=encoded_userid)
 
-    return {"userid": user_create.userid}
+    return {"userid": decoded_userid}
+
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def get_home_page(request: Request, response: Response):
+
     global simulator_manager
-    response.set_cookie(key="est_theta", value=0)
     simulator_manager.initialize_simulator()
-    # 홈 페이지를 렌더링하여 반환합니다.
+
+    # est_theta 쿠키 초기화
+    response.set_cookie(key="est_theta", value=0)
+    
+    # 홈 페이지를 렌더링하여 반환
     return templates.TemplateResponse("home.html", {"request": request})
 
 async def get_next_question():
@@ -178,17 +90,20 @@ async def get_next_question():
     
     return {"item_id": item_id}
 
+
 @app.get("/get-question")
 async def get_question():
     
     # 다음 문제 반환
     return await get_next_question()
 
+
 @app.get("/quiz", response_class=HTMLResponse)
 async def get_quiz_page(request: Request):
     
     # quiz.html을 렌더링하여 반환
     return templates.TemplateResponse("quiz.html", {"request": request})
+
 
 class Answer(BaseModel):
     answer: bool
@@ -205,6 +120,7 @@ class CatResult(Base):
     proficiency = Column(Float, nullable=False)
 
 no = 0
+
 
 # POST 요청 핸들러 함수
 @app.post('/submit-answer')
@@ -246,7 +162,7 @@ async def submit_answer(answer: Answer, response: Response, request: Request, db
         userid=user_id,
         quizcode=json_data['item_ids'][str(simulator_manager.simulator.administered_items[-1])], # 다음 문제의 아이디를 가져와서 사용
         correct=1 if answer.answer else 0, # 정답 여부에 따라 1 또는 0 저장
-        no = no, # userid와 datetime이 같은 경우를 고려하여 no 값 계산
+        no = no,
         datetime=formatted_start_time,  # 시험 시작 시간 사용
         proficiency=proficiency if proficiency is not None else 0
     )
@@ -254,7 +170,8 @@ async def submit_answer(answer: Answer, response: Response, request: Request, db
     db.commit()
 
     if last_quiz:
-        # 마지막 퀴즈인 경우 다음 문제 반환하지 않고, 빈 응답을 보냄
+
+        # 마지막 퀴즈인 경우 다음 문제 반환하지 않고 빈 응답을 보냄
         response.set_cookie(key="est_theta", value=est_theta)
         response.delete_cookie("administered_items")
         response.delete_cookie("responses")
@@ -279,8 +196,10 @@ async def submit_answer(answer: Answer, response: Response, request: Request, db
         response.set_cookie(key="responses", value=responses)
         response.set_cookie(key="index", value=index)
         response.set_cookie(key="last_quiz", value=last_quiz)
+
         if answer.answer:
             return {"message": "정답입니다!", "estimated_proficiency": est_theta, "next_item_id": next_item_id, "last_quiz": last_quiz}
+        
         else:
             return {"message": "오답입니다.", "estimated_proficiency": est_theta, "next_item_id": next_item_id, "last_quiz": last_quiz}
 
@@ -289,5 +208,5 @@ async def submit_answer(answer: Answer, response: Response, request: Request, db
 async def get_result_page(request: Request):
     est_theta = request.cookies.get("est_theta")
     
-    # result.html을 렌더링하여 반환합니다.
+    # result.html을 렌더링하여 반환
     return templates.TemplateResponse("result.html", {"request": request, "est_theta": est_theta})
--- web/simulator.py
+++ web/simulator.py
@@ -1,24 +1,98 @@
+import numpy as np
+from catsim.initialization import FixedPointInitializer
+from catsim.selection import UrrySelector
+from catsim.estimation import NumericalSearchEstimator
+from catsim.stopping import MinErrorStopper
+from load import load_data
+from quiz import json_to_array, get_quiz
+
+    
+initializer = FixedPointInitializer(0)
+selector = UrrySelector()
+estimator = NumericalSearchEstimator()
+stopper = MinErrorStopper(0.6)
+
+json_data = load_data('mathcat-bucket', 'irt_result/yyyy=2024/mm=03/dd=13/irt_result.json')
+result_array = json_to_array(json_data)
+initial_item_ids = get_quiz(json_data, result_array)
+
+
+class SimulatorManager:
+    def __init__(self):
+        self.simulator = None
+    
+    def initialize_simulator(self):
+        self.simulator = Simulator(np.array(result_array), 4, 15, initializer, selector, estimator, stopper)
+
+
 class Simulator:
-    def __init__(self, result_array, max_items, initializer, selector, estimator, stopper):
-        # 초기화 로직
+    def __init__(self, result_array, init_items, max_items, initializer, selector, estimator, stopper):
         self.result_array = result_array
+        self.init_items = init_items
         self.max_items = max_items
         self.initializer = initializer
         self.selector = selector
         self.estimator = estimator
         self.stopper = stopper
+        self.administered_items = []
+        self.responses = []
+        self.index = 0
 
-    def simulate(self, verbose=False):
-        # 여기에 능력치 추정 로직 구현
-        # 예시: 초기 능력치 추정
-        estimated_theta = self.initializer.initialize()
+    def recommend_next(self, user_answer=None, quiz=False):
+        
+        ## 다음 문제 추천
+        # 초기 4문제는 리스트에서 반환
+        if len(self.administered_items) < self.init_items:
+            if quiz:
+                return initial_item_ids[self.index]
 
-        # 시뮬레이션 과정에서 능력치 업데이트
-        # 이 부분에는 반복문과 로직이 필요할 수 있음
-        # 예시 로직:
-        for _ in range(self.max_items):
-            # 실제 구현에서는 여기에 능력치를 업데이트하는 로직이 필요
-            pass
+            item_index = list(json_data['item_ids'].values()).index(initial_item_ids[self.index])
+            self.administered_items.append(item_index)
 
-        # 최종 추정된 능력치 반환
-        return estimated_theta
+            # 문제에 대한 응답 추가
+            correct = user_answer
+            self.responses.append(correct == True)
+            print(f'Did the user answer the item {initial_item_ids[self.index]} correctly?: {self.responses[self.index]}')
+
+
+        # 5번째 문제부터는 사용자의 학습 수준에 맞게 추천
+        else:
+            item_index = self.selector.select(items=self.result_array, administered_items=self.administered_items, est_theta=self.est_theta)
+            if quiz:
+                return json_data['item_ids'][str(item_index)]
+            
+            self.administered_items.append(item_index)
+
+            # 문제에 대한 응답 추가
+            correct = user_answer
+            self.responses.append(correct == True)
+            print(f"Did the user answer the item {json_data['item_ids'][str(item_index)]} correctly?: {self.responses[self.index]}")
+
+
+        # 사용자의 학습 수준 추정
+        if self.index == 3:
+            self.est_theta = self.initializer.initialize()
+            self.est_theta = self.estimator.estimate(
+                items=np.array(self.result_array),
+                administered_items=self.administered_items,
+                response_vector=self.responses,
+                est_theta=self.est_theta
+            )
+        elif self.index > 3:
+            self.est_theta = self.estimator.estimate(
+                items=np.array(self.result_array),
+                administered_items=self.administered_items,
+                response_vector=self.responses,
+                est_theta=self.est_theta
+            )
+        else:
+            self.est_theta = 0
+            
+        if len(self.administered_items) == 15 or self.stopper.stop(administered_items=np.array(self.result_array)[self.administered_items], theta=self.est_theta):
+            # 사용자의 학습 수준 출력
+            print("시험 종료")
+            print("Final estimated proficiency:", self.est_theta)
+            return self.est_theta, self.administered_items, self.responses, self.index, True
+
+        self.index += 1
+        return self.est_theta, self.administered_items, self.responses, self.index, False
